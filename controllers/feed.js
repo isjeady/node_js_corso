4cbad1fe58049129a6fa5dfcbc7cfd34bfb87518
Unknown <vitto.leandro@gmail.com>
2019-04-07 10:54:41 +0200

    Capitolo 3 - CORS & Planning Api

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,41 +1,68 @@
+const { validationResult } = require('express-validator/check');
+
+
 exports.getPosts = (req,res,next) => {
     res.json({ 
         posts : [
-            {
-                id : 3 ,
-                title : 'first' , 
-                description : 'description' 
+            { 
+                id : 1, 
+                title : 'titolo 1' , 
+                description : 'description adf adf af ',
+                image : 'images/1.png',
+                author : {
+                    name : 'Leandro'
+                },
+                creation_date : new Date()
             },
-            {
-                id : 5 ,
-                title : 'first' , 
-                description : 'description' 
+            { 
+                id : 2, 
+                title : 'titolo 1' , 
+                description : 'description adf adf af ',
+                image : 'images/1.png',
+                author : {
+                    name : 'Leandro'
+                },
+                creation_date : new Date()
             },
-            {
-                id : 1323 ,
-                title : 'first' , 
-                description : 'description' 
-            }
+            { 
+                id : 3, 
+                title : 'titolo 1' , 
+                description : 'description adf adf af ',
+                image : 'images/1.png',
+                author : {
+                    name : 'Leandro'
+                },
+                creation_date : new Date()
+            },
+
         ]
     });
 };
 
 
 exports.createPosts = (req,res,next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            message : 'Error input',
+            error : errors.array()
+        });
+    }
+
     const title = req.body.title;
     const description = req.body.description;
 
-    //Salvo nel DB e torno il nuovo ID
-    const ID = 1324; //back to DB
-
+    //SALVO NEL DATABASE e genera ID
+    const ID = 1234;
+    
+    //TODO: test
     res.status(201).json({ 
         messages : 'Success Operation',
-        post : [
-            {
-                id : ID ,
-                title : title , 
-                description : description
-            }
-        ]
+        post : { 
+            id : ID,
+            title : title,
+            description : description
+        }
     });
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,14 +1,38 @@
 const express = require('express');
+const { body } = require('express-validator/check');
 const router = express.Router();
 
 const feedController = require('../controllers/feed');
 
+//POST /feed/posts
+router.post('/post',
+    [
+        body('title').trim().isLength({ min : 3}).exists(),
+        body('description').trim().isLength({ min : 5}),
+    ]
+,feedController.createPosts);
+
+//NO AUTH | AUTH
+//GET ALL /feed/posts
+//GET /feed/post/:id
+
+//POST /feed/post
+//PUT /feed/post/:id
+//DELETE /feed/post/:id
+
+//POST /feed/post/:id/like
+
+//6 END POINT
+//-----> JWT
+//POST /user/login
+//POST /user/register
+//GET  /user/me
+//PUT  /user/:id   //verificare permessi
+
+//4 END POINT
+
+
 router.get('/posts',feedController.getPosts);
-router.post('/posts',feedController.createPosts);
-
-
-
-
 
 
 
