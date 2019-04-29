8cb1e24e28ab4b0771d15db5356ef31b6b30b8d1
Unknown <vitto.leandro@gmail.com>
2019-04-30 00:36:22 +0200

    fix

--- utils/database.js
+++ utils/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(process.env.NODE_DATABASE,
     process.env.NODE_DATABASE_PW,
 {
     dialect : 'mysql',
-    host: 'localhost'
+    host: process.env.NODE_DATABASE_URL
 }
 );
 
